670626ec9aba151a0fc8dbaf001cb1b4316c3191
AJ Guiles <ashiguiles@gmail.com>
2019-03-03 07:08:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,10 @@
 def my_each(array)
-  if block_given?
+ 
     i = 0 
     while < array.length 
     yield(array[i])
-     i = i + 1
+     i += 1
 end
 array
-else
-  puts "NO Block"
+ 
 end
-end 
